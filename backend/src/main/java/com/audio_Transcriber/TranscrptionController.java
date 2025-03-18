1cec371604cd362fd78a1dae82eb5d7747db61f7
Mchedi <miled.chedi@esprit.tn>
2025-03-18 14:26:54 +0100

    final push before docker

--- backend/src/main/java/com/audio_Transcriber/TranscrptionController.java
+++ backend/src/main/java/com/audio_Transcriber/TranscrptionController.java
@@ -1,14 +1,7 @@
 package com.audio_Transcriber;
 
 
-import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
-import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
-import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
-import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
-import org.springframework.ai.openai.api.OpenAiAudioApi;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
+
 import org.springframework.util.LinkedMultiValueMap;
-import java.io.BufferedReader;
-import java.io.File;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/api")
